e9365537eb0dea5d1680806a24e5ff00d2e19ab1
ShuuNae <phuocancntt@gmail.com>
2021-06-07 16:11:31 +0700

    Update dispatches.controller.js

--- api/dispatches/dispatches.controller.js
+++ api/dispatches/dispatches.controller.js
@@ -150,7 +150,7 @@ module.exports = {
       Key: filePath,
       Expires: 60,
       ContentType: fileType,
-      ContentEncoding: "base64",
+      // ContentEncoding: "base64",
       // ACL: "public-read",
     };
 
